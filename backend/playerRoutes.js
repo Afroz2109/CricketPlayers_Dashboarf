6ea1495ef2e44607e4191d02eebacb1be3762774
Afroz2109 <affu962k@gmail.com>
2025-07-18 17:31:40 +0530

    playerRoutes.js

--- backend/playerRoutes.js
+++ backend/playerRoutes.js
@@ -1,13 +1,16 @@
-const express= require('express');
+const express = require('express');
+const {
+  getPlayers,
+  addPlayer,
+  updatePlayer,
+  deletePlayer
+} = require('../controllers/playerController'); // Adjust the path if needed
 
-const playerRouter= express.Router();
+const playerRouter = express.Router();
 
-playerRouter.get('/get-players');
+playerRouter.get('/get-players', getPlayers);
+playerRouter.post('/add-players', addPlayer);
+playerRouter.put('/update-player/:id', updatePlayer);
+playerRouter.delete('/delete-player/:id', deletePlayer);
 
-playerRouter.post('/add-players');
-
-playerRouter.put('/update-player');
-
-playerRouter.delete('/delete-player');
-
-module.exports= playerRouter;
+module.exports = playerRouter;
